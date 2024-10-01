b3c9100c9779c09e100e99d152365d43245ec600
savyobrenner <savyobrenner@gmail.com>
2024-10-01 20:49:20 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Savyo Brenner on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
